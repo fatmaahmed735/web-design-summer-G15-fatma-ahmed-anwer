7357f36619fc30ca8f9c8c05498f2da85b81cf01
fatmaahmed735 <fatmaahmadaaf5@gmail.com>
2026-08-30 10:45:17 +0300

    Add Task10

---
+++ Task10/script.js
@@ -0,0 +1,24 @@
+var s="";
+var degree = prompt("Enter your degree");
+
+if (degree == null) {
+    alert("Please enter your degree");
+}
+else if (degree == "") {
+    alert("Please enter your degree");
+}
+else if (degree > 100) {
+    alert("Invalid degree");
+}
+else if (degree >= 90 && degree <= 100) {
+    alert("Excellent");
+}
+else if (degree >= 80 && degree < 90) {
+    alert("very good");
+}
+else if (degree >= 70 && degree < 80) {
+    alert("good");
+}
+else if (degree < 70) {
+    alert("failed");
+}
