9c60d6c755dfcb2cb9345fd2f6c302db4a41d817
fatmaahmed735 <fatmaahmadaaf5@gmail.com>
2026-09-02 11:04:29 +0300

    Add Task14

---
+++ Task14/script.js
@@ -0,0 +1,44 @@
+let users = [
+    {
+        name: "fatma",
+        email: "fatma@gmail.com"
+    },
+    {
+        name: "Marwa",
+        email: "Marwa@gmail.com"
+    }
+];
+
+localStorage.setItem("users", JSON.stringify(users));
+
+
+
+
+
+
+
+
+
+
+
+
+
+let usersList = JSON.parse(localStorage.getItem("users"));
+
+let container = document.getElementById("users");
+
+usersList.forEach(function(user) {
+
+    let div = document.createElement("div");
+
+    div.className = "col-md-4";
+
+    div.innerHTML = `
+        <div class="card p-3">
+            <h4>${user.name}</h4>
+            <p class="mb-0">${user.email}</p>
+        </div>
+    `;
+
+    container.appendChild(div);
+});
